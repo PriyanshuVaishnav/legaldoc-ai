85a48e52eddeea3f98d8b90747e005b2643be7a0
PriyanshuVaishnav <priyanshuvaishnav2566@gmail.com>
2025-06-09 22:33:59 +0530

    Add files via upload

--- ner_extractor.py
+++ ner_extractor.py
@@ -1,6 +1,12 @@
 import spacy
+import os
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def extract_entities(text):
     doc = nlp(text)
